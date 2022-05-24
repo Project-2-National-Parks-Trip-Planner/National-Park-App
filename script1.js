10f6df04cd52dbbcd6a12ec6d03ba2b1a76aebc9
natalielaing <natalielaing7@gmail.com>
2022-05-24 17:57:09 -0400

    made significant changes to js1 file, to display relevant info on the page

--- script1.js
+++ script1.js
@@ -1,65 +1,157 @@
+//App goals *** 
+//on page load 
+    //use the NPS api to get all park data 
+
+//on submit of form 
+    //1. get the value of the state element 
+    //use that value to get relevant park data from Api 
+    //hold that refined data as a new object 
+    //2. get the value of the budget element 
+    //use that value to get relevant data from new state object 
+    //hold that refined data as new object 
+    //3. get the value of the activity element 
+    //use that value to get relevant data from new object 
+//display title, img, and description on the page of relevant data 
+
+
+
 const parkApp = {};
-
-parkApp.init = function(){
-    //code to kickoff app goes here
-    parkApp.getParks(); 
-};
-
-//test the API to determine whether it's working and to ensure we're getting the expected data back 
 
 //save relevant API info 
 parkApp.apiUrl = "https://developer.nps.gov/api/v1/parks"
 parkApp.apiKey = "sIbVeWuJQGDB0ggBcZ6WYl17B68jGWLccRhQajRG"
+parkApp.form = document.querySelector('#form');
 
-parkApp.getParks = function () {
+
+parkApp.getParks = function (query) {
     //fetch request for park info 
     const url = new URL(parkApp.apiUrl);
     url.search = new URLSearchParams({
         api_key: parkApp.apiKey,
-        limit: 500
+        limit: 500,
+        q: query
     });
 
+    //make our Api call
     fetch(url)
     .then(results => {
         return results.json();
     }).then(parkData =>{
-        const object = parkData.data;
-        console.log(object);
-
-        parkApp.parkActivities(object);
-        parkApp.openingHours(object);
+        // parkApp.parkInfo.innerHTML = '';
+        const parkDataArray = parkData.data;
+        parkApp.displayParks(parkDataArray);
     })
-}
-//RETURNS ARRAY OF 466 PARKS 
 
-parkApp.parkActivities = function (object){
-    const parkActivities = object.map( ({activities}) => {
-        return activities;
-    })
+//         parkApp.filterResults();
+
+//         parkApp.parkActivities(object);
+//         parkApp.openingHours(object);
     
-    console.log(parkActivities);
 }
 
-//RETURNS ACTIVITIES FOR EACH PARK AS NEW ARRAY 
 
 
-parkApp.openingHours = function (object) {
-    const openingHours = object.map( ({operatingHours}) => {
-        return operatingHours;
-    });
-    const daysOpen = openingHours.map( () => {
-        return openingHours[0];
-    });
-    const weekdays = daysOpen.map( () => {
-        return daysOpen[0];
-    })
-    const hours = weekdays.map( ({standardHours}) => {
-        return standardHours;
-    })
+parkApp.displayParks = function(parkDataArray) {
+    for(let i=0, l = parkDataArray.length; i < l; i++) {
+    const object=parkDataArray[i]; 
+    console.log(object);
+    // console.log(object.addresses[0].stateCode); example of how to get state code. 
+    const title = document.createElement ('h2');
+    title.innerText = object.fullName;
 
-    console.log(hours);
+    const image = document.createElement('img');
+    image.src = object.images[0].url;
+    image.alt = object.images[0].altText;
+
+    const description = document.createElement('p');
+    description.innerText = object.description;
+
+    const parkInfo = document.createElement('div');
+    parkInfo.classList.add('info');
+    parkInfo.appendChild(title);
+    parkInfo.appendChild(image);
+    parkInfo.appendChild(description);
+
+    //append HTMl to the #parksInfo element
+    document.querySelector('#parksInfo').appendChild(parkInfo);
+    }
 }
 
+parkApp.eventListener = function () {
+    parkApp.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const userStateChoice = document.querySelector('#chosenState').value;
+
+        const userBudgetChoice = document.querySelector('#budget').value;
+
+        const userActivityChoice = document.querySelector ('#activity').value;
+        console.log(userStateChoice);
+        parkApp.getParks (userStateChoice);
+        
+    })
+}
+
+
+
+
+
+
+
+// //RETURNS ARRAY OF 466 PARKS (object)
+
+// parkApp.parkActivities = function (object){
+//     const parkActivities = object.map( ({activities}) => {
+//         return activities;
+//     })
+    
+//     console.log(parkActivities);
+// }
+
+// //RETURNS ACTIVITIES FOR EACH PARK AS NEW ARRAY 
+
+
+// parkApp.openingHours = function (object) {
+//     const openingHours = object.map( ({operatingHours}) => {
+//         return operatingHours;
+//     });
+//     const daysOpen = openingHours.map( () => {
+//         return openingHours[0];
+//     });
+//     const weekdays = daysOpen.map( () => {
+//         return daysOpen[0];
+//     })
+//     const hours = weekdays.map( ({standardHours}) => {
+//         return standardHours;
+//     })
+
+//     console.log(hours);
+// }
+
+// //function to filter on submit 
+
+// parkApp.filterResults = function (object) {
+//     const submitButton = document.querySelector("button");
+//     submitButton.addEventListener('submit', function (e) {
+//         e.preventDefault();
+//         //filter by state 
+//         //filter by budget
+//         //filter by activities 
+//         alert('you have clicked submit');
+
+
+
+//     })
+// }
+
+//function to display results of filter 
+
+parkApp.init = function(){
+    //code to kickoff app goes here
+    parkApp.eventListener();
+    parkApp.getParks(); 
+    
+};
 
 
 parkApp.init();
