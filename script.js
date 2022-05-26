f7782dbeeb56ef48b8bb6276eb95556fceb2a319
natalielaing <natalielaing7@gmail.com>
2022-05-25 20:34:59 -0400

    deleted extra script file

--- script.js
+++ script.js
@@ -1,74 +1,169 @@
+//App goals *** 
+//on page load 
+    //use the NPS api to get all park data 
+
+//on submit of form 
+    //1. get the value of the state element 
+    //use that value to get relevant park data from Api 
+    //hold that refined data as a new object 
+    //2. get the value of the budget element 
+    //use that value to get relevant data from new state object 
+    //hold that refined data as new object 
+    //3. get the value of the activity element 
+    //use that value to get relevant data from new object 
+//display title, img, and description on the page of relevant data 
 
 
-// create app object
 
 const parkApp = {};
-
-// create an initialization method
-parkApp.init = () => {
-    //calling the method which makes the request to the API
-    parkApp.getData();
-
-    // parkApp.handleOnChangeSelect();
-  }
 
 //save relevant API info 
 parkApp.apiUrl = "https://developer.nps.gov/api/v1/parks"
 parkApp.apiKey = "sIbVeWuJQGDB0ggBcZ6WYl17B68jGWLccRhQajRG"
+parkApp.form = document.querySelector('#form');
 
 
+parkApp.getParks = function (query, query2) {
+    //fetch request for park info 
+    const url = new URL(parkApp.apiUrl);
+    url.search = new URLSearchParams({
+        api_key: parkApp.apiKey,
+        limit: 500,
+        statecode: query,
+        q: query2,
+        // q: "cost:%2030.00"
+    });
 
-// create a method (AKA function on the app object) which requests information from the API
-// logs it to the console
-parkApp.getData = () => {
-  //use the URL constructor to specify the parameters we wish to include in our API endpoint (AKA in the request we are making to the API)
-  const url = new URL(parkApp.apiUrl);
-  
-  url.search = new URLSearchParams({
-    // pass in our API key as a parameter
-    api_key: parkApp.apiKey,
-    limit: 466
-  })
- 
-  
-
-  // using the fetch API to make a request to the NPS Parks API endpoint
-  // pass in new URL featuring params provided by the URLSearchParams constructor
-  fetch(url)
-    .then((response) => {
-      // parse this response into JSON
-      // return JSON response so that it can be used in the next function
-      return response.json();
-    })
-    //parse the JSON Promise response and log out readable data (AKA data in JSON format)
-    .then((jsonResponse) => {
-    console.log(jsonResponse);
+    //make our Api call
+    fetch(url)
+    .then(results => {
+        return results.json();
+    }).then(parkData =>{
+        // parkApp.parkInfo.innerHTML = '';
+        const parkDataArray = parkData.data;
+        parkApp.displayParks(parkDataArray);
     })
 
+//         parkApp.filterResults();
+
+//         parkApp.parkActivities(object);
+//         parkApp.openingHours(object);
+    
 }
 
-// parkApp.activity = '';
 
-// parkApp.filterByActivity = () => {
 
+parkApp.displayParks = function(parkDataArray) {
+    for(let i=0, l = parkDataArray.length; i < l; i++) {
+    const object=parkDataArray[i]; 
+    console.log(object);
+    // console.log(object.addresses[0].stateCode); example of how to get state code. 
+    const title = document.createElement ('h2');
+    title.innerText = object.fullName;
+
+    const image = document.createElement('img');
+    image.src = object.images[0].url;
+    image.alt = object.images[0].altText;
+
+    const description = document.createElement('p');
+    description.innerText = object.description;
+
+    const parkInfo = document.createElement('div');
+    parkInfo.classList.add('info');
+    parkInfo.appendChild(title);
+    parkInfo.appendChild(image);
+    parkInfo.appendChild(description);
+
+    // console.log(displayPark.freeParks);
+
+    // const fees = object.entranceFees[0];
+    // console.log(fees);
+
+    //append HTMl to the #parksInfo element
+    document.querySelector('#parksInfo').appendChild(parkInfo);
+    }
+
+    // const freeParks = object.filter((object) => {
+    //     return park.EntranceFees[0].cost = "0.00";
+    // })
+}
+
+parkApp.eventListener = function () {
+    parkApp.form.addEventListener('submit', function (e) {
+        e.preventDefault();
+
+        const userStateChoice = document.querySelector('#chosenState').value;
+
+        // const userBudgetChoice = document.querySelector('input[name="cost"]:checked').value;
+
+        const userActivityChoice = document.querySelector('#activity').value;
+
+        console.log(userStateChoice, userActivityChoice);
+        parkApp.getParks (userStateChoice, userActivityChoice);
+    })
+    }
+
+
+
+
+
+
+
+// //RETURNS ARRAY OF 466 PARKS (object)
+
+// parkApp.parkActivities = function (object){
+//     const parkActivities = object.map( ({activities}) => {
+//         return activities;
+//     })
+    
+//     console.log(parkActivities);
 // }
 
-// const checkboxActivities = document.querySelectorAll('input[name="activity"]');
-// parkApp.selectedActivity = () => {
-//     if (checkboxActivities === checked)
-//     return true;
+// //RETURNS ACTIVITIES FOR EACH PARK AS NEW ARRAY 
+
+
+// parkApp.openingHours = function (object) {
+//     const openingHours = object.map( ({operatingHours}) => {
+//         return operatingHours;
+//     });
+//     const daysOpen = openingHours.map( () => {
+//         return openingHours[0];
+//     });
+//     const weekdays = daysOpen.map( () => {
+//         return daysOpen[0];
+//     })
+//     const hours = weekdays.map( ({standardHours}) => {
+//         return standardHours;
+//     })
+
+//     console.log(hours);
 // }
 
+// //function to filter on submit 
+
+// parkApp.filterResults = function (object) {
+//     const submitButton = document.querySelector("button");
+//     submitButton.addEventListener('submit', function (e) {
+//         e.preventDefault();
+//         //filter by state 
+//         //filter by budget
+//         //filter by activities 
+//         alert('you have clicked submit');
 
 
-// //call the init method to kickstart our app
+
+//     })
+// }
+
+//function to display results of filter 
+
+parkApp.init = function(){
+    //code to kickoff app goes here
+    parkApp.eventListener();
+    parkApp.getParks(); 
+    
+};
+
+
 parkApp.init();
-
-
-
-
-
-
-// console.log(parkApp);
-
 
