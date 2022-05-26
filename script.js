0088052243da2314ea61ac83e1c9bc3a6bf2a1df
Pau <pauligonzalezincker@gmail.com>
2022-05-25 22:47:48 -0400

    started working on landing page css and js functionality

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-
-
 // create app object
 
 const parkApp = {};
@@ -8,8 +6,7 @@ const parkApp = {};
 parkApp.init = () => {
     //calling the method which makes the request to the API
     parkApp.getData();
-
-    
+  
   }
 
 //save relevant API info 
@@ -17,83 +14,134 @@ parkApp.apiUrl = "https://developer.nps.gov/api/v1/parks"
 parkApp.apiKey = "sIbVeWuJQGDB0ggBcZ6WYl17B68jGWLccRhQajRG"
 
 
-
 // create a method (AKA function on the app object) which requests information from the API
 // logs it to the console
 parkApp.getData = () => {
-  //use the URL constructor to specify the parameters we wish to include in our API endpoint (AKA in the request we are making to the API)
   const url = new URL(parkApp.apiUrl);
   
   url.search = new URLSearchParams({
-    // pass in our API key as a parameter
-    api_key: parkApp.apiKey
+    api_key: parkApp.apiKey,
+    limit: 466,
   })
  
-  
-
-  // using the fetch API to make a request to the NPS Parks API photos endpoint
-  // pass in new URL featuring params provided by the URLSearchParams constructor
   fetch(url)
     .then((response) => {
-      // parse this response into JSON
-      // return JSON response so that it can be used in the next function
       return response.json();
     })
-    //parse the JSON Promise response and log out readable data (AKA data in JSON format)
     .then((jsonResponse) => {
       const data = jsonResponse.data;
-      
-      parkApp.displayStates(data);
+      parkApp.parkStates(data);
     })
-
 }
 
-
-
 parkApp.state = '';
-parkApp.displayStates = function(data) {
+
+parkApp.parkStates = function(data) {
   console.log(data);
-  
-  
-  data.map((item) => {
-    
-    
-  })
-  //QUERY SELECT FROM THE DOM AND ADD ON CHANGE EVENT TO IT
+
   const selectElement = document.getElementById('chosenState');
- 
   selectElement.addEventListener('change', () => {
     const selectedValue = selectElement.value;
-    parkApp.state = selectedValue;
+    parkApp.state = selectedValue;    
 
     //IF MY OBJECT INCLUDES! SELECTED VALUE, GIVE IT TO ME
     parkApp.selectedState = data.filter((item) => {
       return item.states.includes(selectedValue);
     })
 
-    console.log(parkApp.selectedState);
-    
+    parkApp.filterByCost = parkApp.selectedState;
+  
   })
+
+  
+  const formElement = document.getElementById('form');
+  const parkCosts = data[0].entranceFees //array of park fees  
+
+  formElement.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const selectedValue = selectElement.value;
+    parkApp.state = selectedValue;  
+    
+    
+    parkApp.filterByPrice = function(parkData, inputCost) {
+      if (inputCost === 'cheap') {
+        const listOfFees = parkApp.selectedState.map((cheapFee) => {
+          //cheapFee.entranceFees ->  this is the array we need to filter 
+          const filterCheapFee = cheapFee.entranceFees.filter((el) => {
+            return el.cost <= '15' && el.lenght !== 0; // put in my new array property cost less or equal than 15 AND items with something inside (not empty);
+          })
+
+          return filterCheapFee; 
+        })
+        console.log(listOfFees);
+      }
+      else if(inputCost === 'medium') {
+        const listOfFees = parkApp.selectedState.map((mediumFee) => {
+          return mediumFee;
+        })
+        console.log(listOfFees);
+      }else if(inputCost === 'expensive' ) {
+        const listOfFees = parkApp.selectedState.map((expensiveFee) => {
+          return expensiveFee;
+        })
+        console.log(listOfFees); 
+      }
+      
+      
+      
+      
+    }
+    
+    
+
+
+    //IF MY OBJECT INCLUDES! SELECTED VALUE, GIVE IT TO ME
+    parkApp.selectedState = data.filter((item) => {
+      return item.states.includes(selectedValue);
+    })
+
+    parkApp.filterByCost = parkApp.selectedState;
+
+
+    
+
+    console.log('Array from state filter ', parkApp.selectedState);
+
+    const inputCost = document.querySelector('input[name = "price"]:checked').value;
+    console.log(inputCost);
+    
+    parkApp.filterByPrice(parkApp.selectedState, inputCost);
+
+  })
+
+
+
+  
+
+
+
 
   //PARK NAMES
-  const parkNames = data.map((name) => {
-    return name.fullName;
-  })
-  console.log(parkNames);
+  // const parkNames = data.map((name) => {
+  //   return name.fullName;
+  // })
+  // console.log(parkNames);
 
   //PARK ACTIVITIES
-  const parkActivities = data.map((activitie) => {
-    return activitie.activities;
-  })
-  console.log(parkActivities);
+  // const parkActivities = data.map((activitie) => {
+  //   // return activitie.activities;
+  // })
+  // console.log(parkActivities);
 
   //PARK FEES
-  const parkFees = data.map((fee) => {
-    return fee.entranceFees;
-  })
-  console.log(parkFees);
+  // const parkFees = data.map((fee) => {
+  //   return fee.entranceFees;
+  // })
+  // console.log(parkFees);
   
 } 
+
+
 
 
 
@@ -104,6 +152,6 @@ parkApp.displayStates = function(data) {
 //call the init method to kickstart our app
 parkApp.init();
 
-console.log(parkApp);
+// console.log(parkApp);
 
 
