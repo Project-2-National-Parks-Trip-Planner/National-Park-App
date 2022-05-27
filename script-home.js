860e25721afb61235221c9f9f113bec29f1824b3
Pau <pauligonzalezincker@gmail.com>
2022-05-27 09:43:26 -0400

    landing page functionality almost done

--- script-home.js
+++ script-home.js
@@ -1,10 +1,45 @@
 const body = document.body;
-const homeImage = document.querySelectorAll('.homeImage');
-const leftBtn = document.getElementById('.left');
-const rightBtn = document.getElementById('.right');
+const homeImages = document.querySelectorAll('.homeImage');
+const leftBtn = document.getElementById('left');
+const rightBtn = document.getElementById('right');
 
-let activeSlide = 0;
+let activeImage = 0;
 
-// function setBackground() {
-//     body.style.backgroundImage = slides[activeSlide]
-// }
+
+rightBtn.addEventListener('click', () => {
+    activeImage++
+
+    if(activeImage > homeImages.length - 1) {
+        activeImage = 0
+    }
+
+    setBackground();
+    setActiveImage();
+})
+
+leftBtn.addEventListener('click', () => {
+    activeImage--
+
+    if(activeImage < 0) {
+        activeImage = homeImages.length - 1
+    }
+
+    setBackground();
+    setActiveImage();
+})
+
+
+setBackground();
+
+function setBackground() {
+    body.style.backgroundImage = homeImages[activeImage].style.backgroundImage;
+    console.log(body.style.backgroundImage);
+    
+}
+
+function setActiveImage() {
+    homeImages.forEach((homeImage) => {
+        homeImage.classList.remove('active')
+    })
+    homeImages[activeImage].classList.add('active')
+}
