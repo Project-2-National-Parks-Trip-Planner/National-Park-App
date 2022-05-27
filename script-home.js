8a24a4419ab3ed2364700c9a8f7a1ec4f1ba9d91
Pau <pauligonzalezincker@gmail.com>
2022-05-26 21:16:36 -0400

    updated home/landing page js

---
+++ script-home.js
@@ -0,0 +1,10 @@
+const body = document.body;
+const homeImage = document.querySelectorAll('.homeImage');
+const leftBtn = document.getElementById('.left');
+const rightBtn = document.getElementById('.right');
+
+let activeSlide = 0;
+
+// function setBackground() {
+//     body.style.backgroundImage = slides[activeSlide]
+// }
